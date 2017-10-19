0bf872548921152bd5945e2322542b19027a701d
Sayyid Dzikhri K.A.I <sayyid.ilmy@gmail.com>
2017-10-19 19:49:26 +0800

    v3

--- admin/modules/circulation/submenu.php
+++ admin/modules/circulation/submenu.php
@@ -30,4 +30,5 @@ $menu[] = array(__('Loan Rules'), MWB.'circulation/loan_rules.php', __('View and
 $menu[] = array(__('Loan History'), MWB.'reporting/customs/loan_history.php', __('Loan History Overview'));
 $menu[] = array(__('Overdued List'), MWB.'reporting/customs/overdued_list.php', __('View Members Having Overdues'));
 $menu[] = array(__('Overdued List By Member'), MWB.'reporting/customs/overdued_list_bymember.php', __('View Members Having Overdues'));
+$menu[] = array(__('Overdued List By Student'), MWB.'reporting/customs/overdued_list_bystudent.php', __('View Student Having Overdues'));
 $menu[] = array(__('Reservation'), MWB.'reporting/customs/reserve_list.php', __('Reservation'));
